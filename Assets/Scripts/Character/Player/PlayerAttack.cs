110c37977b372708aefcda97d87cd76843ca219b
HwangHyebin <gpqls0387@gmail.com>
2016-07-18 17:15:27 +0900

    160718, 160715기준 변경된 것 올림

--- Assets/Scripts/Character/Player/PlayerAttack.cs
+++ Assets/Scripts/Character/Player/PlayerAttack.cs
@@ -54,25 +54,56 @@ public class PlayerAttack : PlayerState
                 startTime       = Time.time;
                 float delay     = 1.0f;
                 nextTime        = startTime + delay;
-                RaycastHit hit;
-                Ray ray = new Ray(_player.transform.position + _player.transform.up * 0.25f, _player.transform.forward);
-                Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 100);
+                //각도 안에 들어가고 attack 했을때 몬스터 피 깎아주게만들기.
+                //사이각 구하기
+                //RaycastHit hit;
+                //Ray ray = new Ray(_player.transform.position + _player.transform.up * 0.25f, _player.transform.forward);
+                //Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 100);
              
-                if (Physics.Raycast(ray, out hit, 4))
-                {
-                    // hp 줄여주는 부분
-                    Transform hp = hit.transform.parent.FindChild("HP");
-                    if (hp != null)
-                    {
-                        Life life = hp.GetComponent<Life>();
-                        life.m_HP -= 10;
-                        //파티클 터지는 것 넣어줄 부분.
-                    }
-                    else
-                    {
-                        _player.Get_Enemy.enemy_anim = Enemy.ENEMY_ANIMATOR.DEAD;
-                    }
-                }
+                //if (Physics.Raycast(ray, out hit, 4))
+                //{
+                //    // hp 줄여주는 부분
+                //    Transform hp = hit.transform.parent.FindChild("HP");
+                //    if (hp != null)
+                //    {
+                //        Life life = hp.GetComponent<Life>();
+                //        life.m_HP -= 10;
+                //        //파티클 터지는 것 넣어줄 부분.
+                //    }
+                //    else
+                //    {
+                //        _player.Get_Enemy.enemy_anim = Enemy.ENEMY_ANIMATOR.DEAD;
+                //    }
+                //}
+                Vector3 _forward = _player.transform.forward;
+                Vector3 _right = _player.transform.right;
+                float deg = Mathf.Acos(Vector3.Dot(_forward, _right)) * Mathf.Rad2Deg;
+                Debug.Log("player deg = "+deg);
+                //리스트 for문을 돌려서 각각의 몬스터의 벡터를 구함.
+
+				for (int i = 0; i < _player.Get_Gamemanager._enemyArray.Length; ++i) 
+				{
+					//enemyArray가 null임.
+					if ( _player.Get_Gamemanager._enemyArray[i] != null) 
+					{
+						Vector3 pos =  _player.Get_Gamemanager._enemyArray [i].transform.position - _player.transform.position;
+						float enemy_deg = Mathf.Acos(Vector3.Dot(_forward, pos)) * Mathf.Rad2Deg;
+						if (enemy_deg < deg) 
+						{
+							Transform hp = _player.Get_Enemy.transform.parent.FindChild("HP");
+							if (hp != null)
+							{
+								Life life = hp.GetComponent<Life>();
+								life.m_HP -= 10;
+								//파티클 터지는 것 넣어줄 부분.
+							}
+							else
+							{
+								_player.Get_Enemy.enemy_anim = Enemy.ENEMY_ANIMATOR.DEAD;
+							}
+						}
+					}
+				}   
             }
 
             if (Time.time > nextTime && startTime != 0) //1초 후
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
-    private CharacterBase[]     _array;
+    private CharacterBase[]     _array; //포인터를 담기 위한 배열
     private ControlCamera       _camera;
     private MapFactory          _map;
     private CharacterFactory    _factory;
+
+	[HideInInspector] 
+	public 	CharacterBase[]     _enemyArray = null;
 
     //나중에 xml 파싱을 통해 캐릭터의 수와 오브젝트의 수 등을 결정.
     private void Awake()
     {
         BaseInstance(3);
+
+		//현재는 2마리로 정해져 있으나 나중엔 xml파싱을 통해 갯수결정; //enemy만을 담기 위한 배열
+		_enemyArray = new CharacterBase[2]; 
+
         _map        = GameObject.FindObjectOfType<MapFactory>();
         _factory    = GameObject.FindObjectOfType<CharacterFactory>();
         _camera     = GameObject.FindObjectOfType<ControlCamera>();
@@ -20,9 +27,8 @@ public class GameManager : MonoBehaviour
     {
         _camera.init();
         _map.CreateMap();
-        _map.CreateObstacle(10);
+        _map.CreateObstacle(10); //오브젝트를 몇개나 생성할 것인지?
         CharacterInit();
-       
     }
     private void Update()
     {
@@ -47,14 +53,17 @@ public class GameManager : MonoBehaviour
     private void CharacterInit()
     {
         _factory.CreateCharacter("Player", ref _array[0]);
-        _factory.CreateCharacter("Enemy", ref _array[1]);
-        _factory.CreateCharacter("Enemy", ref _array[2]);
+
+		for (int i = 0; i < _enemyArray.Length; ++i)
+        {
+			CharacterBase a = _factory.CreateCharacter ("Enemy", ref _array [i + 1]);
+			Debug.Log (a);
+			_enemyArray[i] = a;
+        }
 
         for (int i = 0; i < _array.Length; ++i)
         {
             _array[i].init();
         }
     }
-   
-
 }
